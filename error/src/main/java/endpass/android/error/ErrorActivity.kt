9f79bd5d1f4e1e3d20f581bf0b423e8b271eb3a5
Meirlen <miko_982@mail.ru>
2020-06-29 21:16:35 +0600

    Initial commit

---
+++ error/src/main/java/endpass/android/error/ErrorActivity.kt
@@ -0,0 +1,12 @@
+package endpass.android.error
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ErrorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+    }
+}
