f56ff0de6f62ef1b39904b29de4096d093d6d1ee
Meirlen <miko_982@mail.ru>
2020-06-30 15:40:24 +0600

    changed proguard

--- app/src/main/java/endpass/android/myapplication/MainActivity.kt
+++ app/src/main/java/endpass/android/myapplication/MainActivity.kt
@@ -2,16 +2,12 @@ package endpass.android.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import endpass.android.endpass_sdk.presentation.ext.toast
-import endpass.android.endpass_sdk.presentation.utils.AppSingleton
-import endpass.android.endpass_sdk.presentation.utils.LocalData
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toast(AppSingleton.clientId.toString())
+        //toast(AppSingleton.clientId.toString())
     }
 }
--- endpass_sdk/proguard-rules.pro
+++ endpass_sdk/proguard-rules.pro
@@ -21,4 +21,3 @@
 #-renamesourcefileattribute SourceFile
 
 
--keep class androidx.appcompat.widget.** { *; }
